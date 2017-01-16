0d8ecc38914eb04aa7cc34c204a4f004bfee5d55
TiaraMF <tia.fowers@gmail.com>
2017-01-16 10:20:37 -0700

    Learn Ruby The Hard Way Ex 1-6

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+name = 'Paul'
+age = 41 # single and ready to mingle
+height = 72 # inches
+weight = 170 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
